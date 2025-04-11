aae063dd97851f2508e78a1be71cb675ef678c1b
sihab934 <sihab.info934@gmail.com>
2025-04-11 21:42:09 +0600

    All files commited

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/*menu for mobile devices */
+$(document).ready(function(){
+  
+    // Initialize center mode carousel with responsive settings
+    $(".card-list").slick({
+    slidesToShow: 1,
+    arrows: false,
+    cssEase: 'linear',
+    });
+    });
